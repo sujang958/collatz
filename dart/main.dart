becdbec6d7fb5eb4cdcf47ed3f3b7142fdc0e152
sujang958 <enter7377@naver.com>
2022-06-25 22:45:19 +0900

    Added the dart

---
+++ dart/main.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+void main() {
+  for (var i = 2; i >= 0; i++) {
+    print('$i - ${t(i.toDouble())}');
+  }
+}
+
+double t(double n) {
+  if (n == 1) return n;
+
+  if (n % 2 == 0) {
+    return t(n/2);
+  } else {
+    return t(n * 3 + 1);
+  }
+}
